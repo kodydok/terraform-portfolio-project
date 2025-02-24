19791c4bba6b8c7b76c8e5b3552f2a84e5b3719e
Kody Ma <bykodyma@gmail.com>
2025-02-24 18:30:51 -0500

    changed s3 bucket and dynamodb table in state.tf. added cloudfront to main.tf.

--- km-blog/terraform-nextjs/main.tf
+++ km-blog/terraform-nextjs/main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block"
 resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
   
   depends_on = [ 
-    aws_s3_bucket_ownership_controls.nextjs,
+    aws_s3_bucket_ownership_controls.nextjs_bucket_ownership_controls,
     aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
    ]
 
@@ -43,8 +43,8 @@ resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
 resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
-  policy = jsondecode(({
-    version = "2012-10-17"
+  policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
         Sid = "PublicReadGetObject"
@@ -54,5 +54,56 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
         Resource = "${aws_s3_bucket.nextjs_bucket.arn}/*"
       }
     ]
-  }))
+  })
+}
+
+# origin access identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for Next.JS portfolio site"
+}
+
+# cloudfront distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name
+    origin_id = "S3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+  
+  enabled = true
+  is_ipv6_enabled = true
+  comment = "Next.js portfolio site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD", "OPTIONS"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = "S3-nextjs-portfolio-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
 }
